27722655d86689e2572bc593d71c44fbae42bb6e
levideutsch <levideutsch@live.com>
2022-10-03 20:50:12 -0400

    commit tree 45bb27cbdb79f6dd4684d5a970d37e7daa371627

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() { // This will wait for DOM to load then load itself
+    document.getElementById("text").textContent = "This is really cool!"
+
+  });
+
+//   console.log(
+//     "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+//   ); // This will load first
+
